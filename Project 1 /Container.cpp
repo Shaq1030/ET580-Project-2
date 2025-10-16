0a7be855aa715536af9033438e30ce70ab0b7770
ErnTKE <ernesth332@gmail.com>
2025-10-16 02:17:02 +0000

    Ernest First Commit

--- Project 1 /Container.cpp
+++ Project 1 /Container.cpp
@@ -22,4 +22,22 @@ data = nullptr; // set pointer to null
 bool intArray::empty() const
 {
 return numValues == 0; // true if no elements
+
+//(Ernest First) Remove all array values so that the number of values is zero.
+void clear()
+sz = 0;
 }
+
+// Add value to the end of the array, doubling capacity if full.
+void push_back(const T& value) {
+if (sz == cap) reserve(cap * 2); // Double the capacity if needed
+arr[sz++] = value;
+}
+
+// Print all array values to the console on one line.
+void print() const {
+for (int i = 0; i < sz; ++i)
+std::cout << arr[i] << " ";
+std::cout << std::endl;
+}
+
